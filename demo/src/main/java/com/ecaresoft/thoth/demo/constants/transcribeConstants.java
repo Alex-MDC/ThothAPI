e3bf08f45f050c9cdbe8c52eccba1c627d0a0f68
Aesir <a00824388@itesm.mx>
2022-08-30 15:20:53 -0500

    fixed sonarLint smell on util class

--- demo/src/main/java/com/ecaresoft/thoth/demo/constants/transcribeConstants.java
+++ demo/src/main/java/com/ecaresoft/thoth/demo/constants/transcribeConstants.java
@@ -1,6 +1,10 @@
 package com.ecaresoft.thoth.demo.constants;
 
 public class transcribeConstants {
+
+    private transcribeConstants(){
+        throw new IllegalStateException("Utility class");
+    }
     public static final String SPECIALTY = "PRIMARYCARE";
     public static final String TYPE = "DICTATION";
     public static final String LAN_CODE = "en-US";
